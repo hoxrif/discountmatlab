61297c0bc3343b10f706ba054db837aececd20c0
mustafa terzi <hoxrif@yandex.com>
2023-07-19 11:52:58 +0300

    its not necessary

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hoxrif/Documents/discountmatlab
-# Build directory: /home/hoxrif/Documents/discountmatlab/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
